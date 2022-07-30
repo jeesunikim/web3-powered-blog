8c7a2907422a9a06f177cb266ffddb351d2ece74
Jeesun Kim <jeesunikim@users.noreply.github.com>
2022-07-30 17:50:36 -0400

    remove unused imports

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,16 +1,4 @@
-import utilStyles from "styles/utils.module.css";
-import shopStyles from "styles/shop.module.css";
-
-import BigNumber from "bignumber.js";
 import Head from "next/head";
-import Image from "next/image";
-import { useSelector } from "react-redux";
-
-import { config } from "config/config_testnet";
-import products from "data/products.json";
-
-import { RootState, useAppDispatch } from "store";
-import { sendPayment } from "store/slices/paymentSlice";
 
 import { Layout } from "components/layout";
 
